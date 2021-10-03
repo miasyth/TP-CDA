a78fe7a58ba52d155e6c8af650d604770e051a0c
Corentin Fouquet <corentinfouquet62@gmail.com>
2021-10-03 15:13:04 +0200

    ArrayExemples  03/10 update

--- PHP/ExosArray/ArrayExemples.php
+++ PHP/ExosArray/ArrayExemples.php
@@ -50,6 +50,38 @@ foreach($empty as $v){ // affichage tableau 2 dimensions
 
 echo("\n");
 
+// exemple 4
+
+$size2=5;
+
+$empty=[];
+
+for ($i=0 ; $i<=$size2 ; $i++){
+    for($j=0 ; $j<=$size2 ; $j++){
+       $empty[$i][$j]="0";
+    }
+}
+
+//print_r($empty);
+
+// exemple 5
+
+$array=[];
+
+$array= [[0,1] , [2,3]];
+
+$pcount=count($array);
+
+echo("array:\n");
+for ($i=0 ; $i<$pcount ; $i++){
+    for($j=0 ; $j<$pcount ; $j++){
+        echo($j==$pcount-1) ? "|".$array[$i][$j]."|" : "\n|".$array[$i][$j];
+    }
+}
+
+
+//print_r($empty);
+
 //print_r($array);
 
 /*
